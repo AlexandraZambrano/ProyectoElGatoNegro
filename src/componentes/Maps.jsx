fc80ac3529506e5b48de69c6e0d7558862e53943
christinap <christypm24@gmail.com>
2022-09-07 14:12:07 +0200

    mapa

---
+++ src/componentes/Maps.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { GoogleMap, Marker, useJsApiLoader } from '@react-google-maps/api';
+
+
+
+function MyComponent() {
+  const containerStyle = {
+  width: '500px',
+  height: '500px'
+};
+
+const center = {
+  lat: 40.55747914217795, 
+  lng: -3.644809376466913
+};
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: "AIzaSyDhnNN-xsOhBmJ2-MnDhyiOOn-l91Sf440"
+  })
+
+  const [map, setMap] = React.useState(null)
+
+  const onLoad = React.useCallback(function callback(map) {
+    const bounds = new window.google.maps.LatLngBounds();
+    map.fitBounds(bounds);
+    setMap({center})
+  }, [])
+
+  const onUnmount = React.useCallback(function callback(map) {
+    setMap ({center})
+  }, [])
+
+  return isLoaded ? (
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={18}
+       
+        onUnmount={onUnmount}
+      >
+        { /* Child components, such as markers, info windows, etc. */ }
+        <></>
+      </GoogleMap>
+  ) : <></>
+}
+
+export default React.memo(MyComponent)
