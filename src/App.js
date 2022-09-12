f963fe7fff72d1c7220e318f0695bc752e87ec32
Alexandra Zambrano <alezambrano03@gmail.com>
2022-09-12 14:06:43 +0200

    hola

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
+import React from 'react'
 import './App.css';
-import SeccionMapa from './componentes/SeccionMapa';
+import SeccionMapa from './componentes/SeccionMapa.jsx';
 
 function App() {
   return (
