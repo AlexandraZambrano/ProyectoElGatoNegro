d5be71f2fecd3da8a39f436caaa0896f9908b4c3
ANA KAREN ARAGON VAZQUEZ <anaaragon@MacBook-Pro-de-ANA-2.local>
2022-09-12 14:02:06 +0200

    Hero y footer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import './App.css';
 import './hojas-de-estilo/Boton.css';
 import Hero from './componentes/Hero';
+import Footer from './componentes/Footer';
 
 function App() {
   return (
+    <div className='App'>
+      <div className='Hero'>
+        <Hero />
+      </div>
 
-
-
-    <div className='Hero'>
-      <Hero />
+      <p> hola</p>
+      <div className='Footer'>
+        <Footer />
+      </div>
     </div>
   );
 }
