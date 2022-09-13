04b56b341b06e81e250f4da105189274b3a7a841
Alexandra Zambrano <alezambrano03@gmail.com>
2022-09-13 12:27:48 +0200

    deleted

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
 import './App.css';
-import './hojas-de-estilo/Boton.css';
-import Hero from './componentes/Hero';
-import Footer from './componentes/Footer';
 
 function App() {
   return (
     <div className='App'>
-      <div className='Hero'>
-        <Hero />
-      </div>
 
-      <p> hola</p>
-      <div className='Footer'>
-        <Footer />
-      </div>
+
     </div>
   );
 }
