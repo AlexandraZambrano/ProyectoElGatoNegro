7dab366ba62de915ef697c6270acb44dea9964a5
Alexandra Zambrano <alezambrano03@gmail.com>
2022-09-13 10:53:08 +0200

    App clear

--- src/App.js
+++ src/App.js
@@ -1,20 +1,9 @@
-import React from 'react';
 import './App.css';
-import Galeria from './componentes/Galeria';
-import Navbar from './componentes/Navbar.jsx';
 
 
 function App() {
   return (
     <div className="App">
-
-      <div className='nav'>
-      <Navbar />
-      </div>
-
-      <Galeria />
-
-
 
     </div>
   );
