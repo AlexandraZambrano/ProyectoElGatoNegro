3cc84ae5a03cd32c83b3fa1dd48658616306eb6a
chantell <chantellgebauer@gmail.com>
2022-09-13 17:36:00 +0200

    cookies

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
-import { render, screen } from '@testing-library/react';
+/*/import { render, screen } from '@testing-library/react';
+import CookieConsent, { Cookies } from "react-cookie-consent";
 import App from './App';
 
+describe('Componente FormMasters', () => {
+
+// Prueba en la que busca un texto que no está
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+  // Arrange
+  render(<CookieConsent />);
+  
+  // Act ... Nothing
+
+  // Assert
+  const elementoPrueba = screen.getByText('Este sitio web utiliza cookies para mejorar la experiencia de usuario.');
+  expect(elementoPrueba).toBeInTheDocument();
+});//end test
