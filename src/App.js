c19cea4ec4208109ac072634fcd1dfeff4221031
christinap <christypm24@gmail.com>
2022-09-13 13:25:58 +0200

    App

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import SeccionMapa from './componentes/SeccionMapa'
+
+function App() {
+  return (
+    <div>
+        <SeccionMapa />
+    </div>
+  )
+}
+
+export default App
