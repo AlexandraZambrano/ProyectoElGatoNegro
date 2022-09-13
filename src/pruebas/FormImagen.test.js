b8fcef2e85c1e82d751977eb7f1126c20e9cc431
Cristina Sanjuan <cristy.si@gmail.com>
2022-09-13 20:40:28 +0200

    cambios css margen

--- src/pruebas/FormImagen.test.js
+++ src/pruebas/FormImagen.test.js
@@ -3,9 +3,9 @@ import FormImagen from '../componentes/FormImagen';
 
 
 
-describe('Componente FormImagen', () => {
-  //Prueba que verifica si la imagen se renderiza
-  test('renders learn react link', () => {
+describe('Componente App', () => {
+  //Prueba que verifica si el componente se renderiza por textos
+  test('', () => {
     render(<FormImagen />);
     const compImagen = screen.getByAltText("gato");
     expect(compImagen).toBeInTheDocument();
