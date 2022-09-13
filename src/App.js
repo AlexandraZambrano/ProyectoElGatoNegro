afda04ecf94f51f6191e8276320c09ebad70e333
Cristina Sanjuan <cristy.si@gmail.com>
2022-09-13 12:04:41 +0200

    dep

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Navbar from './componentes/Navbar';
 import Galeria from './componentes/Galeria'
 import Card from './componentes/Card';
 import Popular from './componentes/Popular';
+import FormImagen from './componentes/FormImagen';
+
 
 function App() {
   return (
@@ -13,6 +15,7 @@ function App() {
     <Popular />
     <Card />
     <Galeria />
+    <FormImagen />
     </div>
   );
 }
